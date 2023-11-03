eb1a0a44117171f908369e49e47ba8d8a0fb5886
Muchamad Zainuri <muchamadzainuri6@gmail.com>
2023-11-03 10:50:39 +0700

    Task Pertemuan-6

--- rest-api/app/Http/Controllers/StudentController.php
+++ rest-api/app/Http/Controllers/StudentController.php
@@ -9,9 +9,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class StudentController extends Controller
 {
     // Method atau function untuk menampilkan semua data siswa
-    public function index(){
+    public function index()
+    {
         // menggunakan eloquent all() untuk mengambil semua data siswa pada Model Student
         $students = Student::all();
+
+        // jika data kosong maka kirim status code 204
+        if ($students->isEmpty()) {
+            $data = ["message" => "Resource is empty"];
+
+            return response()->json($data, 204);
+        }
 
         // membuat data response
         $data = [
@@ -26,6 +34,15 @@ class StudentController extends Controller
     // Method atau function untuk menambahkan data siswa baru
     public function store(Request $request)
     {
+
+        // validasi data request
+        $request->validate([
+            "nama" => "required",
+            "nim" => "required",
+            "email" => "required|email",
+            "jurusan" => "required"
+        ]);
+
         // data yang akan ditambahkan
         $input = [
             'nama' => $request->nama,
@@ -48,62 +65,85 @@ class StudentController extends Controller
     }
 
     // Method atau function untuk mengubah data siswa
-    public function update (Request $request, $id){
+    public function update(Request $request, $id)
+    {
         // menggunakan eloquent find() untuk mencari data siswa berdasarkan id
         $student = Student::find($id);
-            // menggunakan eloquent update() untuk mengubah data siswa dan menampilkan kembali menggunakan eloquent all()
-            $student->update($request->all());
 
-            // membuat data response
+        // jika data yang dicari tidak ada, kirim kode 404
+        if (!$student) {
             $data = [
-                "message" => "Student is updated successfully",
-                "data" => $student,
+                "message" => "Data to found",
             ];
 
-            // mengirim data (json) dan kode 200
-            return response()->json($data, 200);
+            return response()->json($data, 404);
+        }
+
+        // menggunakan eloquent update() untuk mengubah data siswa dan menampilkan kembali menggunakan eloquent all()
+        $student->update([
+            "nama" => $request->nama ?? $student->nama,
+            "nim" => $request->nim ?? $student->nim,
+            "email" => $request->email ?? $student->email,
+            "jurusan" => $request->jurusan ?? $student->jurusan,
+        ]);
+        // membuat data response
+        $data = [
+            "message" => "Student is updated successfully",
+            "data" => $student,
+        ];
+        // mengirim data (json) dan kode 200
+        return response()->json($data, 200);
     }
 
     // Method atau function untuk menghapus data siswa
-    public function delete($id){
+    public function delete($id)
+    {
         // menggunakan eloquent find() untuk mencari data siswa berdasarkan id
         $student = Student::find($id);
-            // jika data siswa tidak ditemukan
-            if(!$student){
-                // membuat response data
-                $data = [
-                    "message" => "Student with id $id is not found",
-                ];
-                // mengirim data (json) dan kode 404
-                return response()->json($data, 404);
-            }
-
-            // jika data siswa ditemukan maka hapus data siswa
-            $student->delete();
-
-            // membuat data response
+        // jika data siswa tidak ditemukan
+        if (!$student) {
+            // membuat response data
             $data = [
-                "message" => "Student is deleted successfully",
-                "dataDeleted" => $student,
+                "message" => "Data to found",
             ];
+            // mengirim data (json) dan kode 404
+            return response()->json($data, 404);
+        }
 
-            // mengirim data (json) dan kode 200
-            return response()->json($data, 200);
+        // jika data siswa ditemukan maka hapus data siswa
+        $student->delete();
+
+        // membuat data response
+        $data = [
+            "message" => "Student is deleted successfully",
+            "dataDeleted" => $student,
+        ];
+
+        // mengirim data (json) dan kode 200
+        return response()->json($data, 200);
     }
 
     // Method atau function untuk menampilkan data siswa berdasarkan id
-    public function spesifik($id){
+    public function spesifik($id)
+    {
         // menggunakan eloquent find() untuk mencari data siswa berdasarkan id
         $student = Student::find($id);
 
-            // membuat data response
+        // jika data tidak ada maka kirim status code 404
+        if (!$student) {
             $data = [
-                "message" => "Get student by specific id",
-                "data" => $student,
+                "message" => "Data to found",
             ];
 
-            // mengirim data (json) dan kode 200
-            return response()->json($data, 200);
-    }
+            return response()->json($data, 404);
+        }
+        // membuat data response
+        $data = [
+            "message" => "Get student by specific id",
+            "data" => $student,
+        ];
 
+        // mengirim data (json) dan kode 200
+        return response()->json($data, 200);
+    }
 }
